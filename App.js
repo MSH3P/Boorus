756b97c31912fb24cb75fa7c423a08e2e7b039b1
Grae <greypaper@live.ca>
2020-10-19 20:45:57 -0400

    Added NavigationContainer to app

--- App.js
+++ App.js
@@ -1,16 +1,26 @@
-import React from "react";
+import "react-native-gesture-handler";
+import * as React from "react";
+import { View, Text } from "react-native";
 import { StyleSheet, SafeAreaView, StatusBar } from "react-native";
 
 import Content from "./components/Content.js";
 import Footer from "./components/Footer.js";
 import Navigation from "./components/Navigation.js";
-const App = () => {
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+const Main = () => {
     return (
-        <SafeAreaView style={styles.container}>
-            <StatusBar
-                backgroundColor="#1c2731"
-                barStyle="light-content"
-            ></StatusBar>
+        <SafeAreaView
+            style={{
+                flex: 1,
+                flexDirection: "column",
+                backgroundColor: "#0E1419",
+            }}
+        >
             <Navigation />
             <Content />
             <Footer />
@@ -18,12 +28,14 @@ const App = () => {
     );
 };
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: "column",
-        backgroundColor: "#0E1419",
-    },
-});
-
+const App = () => {
+    return (
+        <NavigationContainer>
+            <StatusBar backgroundColor="#1c2731" barStyle="light-content" />
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Main" component={Main} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
 export default App;
