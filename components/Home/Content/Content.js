c76c4a6f270d5ab618ba2b359477ac0af7f72349
Grae <greypaper@live.ca>
2020-11-10 20:13:06 -0500

    implemented content loading.

--- components/Home/Content/Content.js
+++ components/Home/Content/Content.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, ScrollView, Text, View, Image } from "react-native";
+import { website, tag } from "../../../local/website";
 
 const getContent = (search) => {
-  const limit = 2;
-  const url = `/post/index.json?limit=${limit}&tags=${search}`;
-  const tags = "smell";
+  const limit = 30;
+  const url = `${website}/post/index.json?limit=${limit}&tags=${search || tag}`;
 
   return fetch(url)
     .then((response) => response.json())
@@ -14,40 +14,40 @@ const getContent = (search) => {
       });
     })
     .catch((error) => {
-      console.log("getContent error");
+      console.log("getContent error: " + error);
     });
 };
 
 const Content = (props) => {
   const [data, setData] = useState("");
-  // useEffect(() => {
-  //   getContent(props.search).then((sifted) => {
-  //     const ContentItems = sifted.map((object) => {
-  //       return <View></View>;
-  //     });
-  //   });
-  // }, [props.search]);
+  useEffect(() => {
+    getContent(props.search).then((sifted) => {
+      const ContentItems = sifted.map((object) => {
+        return (
+          <View style={styles.viewBox}>
+            <Image
+              style={styles.viewBox}
+              source={{ uri: object.thumbnail }}
+            ></Image>
+          </View>
+        );
+      });
+      setData(ContentItems);
+    });
+  }, [props.search]);
 
   return (
-    <ScrollView style={{ margin: 5 }}>
+    <ScrollView>
       <View
         style={{
-          height: 150,
-          width: 150,
-          overflow: "hidden",
-          backgroundColor: "cyan",
-          borderRadius: 10,
+          margin: 5,
+          display: "flex",
+          flexWrap: "wrap",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
         }}
       >
-        <Image
-          style={{
-            marginLeft: "auto",
-            marginRight: "auto",
-            height: 150,
-            width: 150,
-          }}
-          source={{ uri: data.preview_url }}
-        />
+        {data || []}
       </View>
     </ScrollView>
   );
@@ -63,6 +63,20 @@ const styles = StyleSheet.create({
     padding: 10,
     marginLeft: 0,
     marginRight: 10,
+  },
+  viewBox: {
+    height: 150,
+    width: 150,
+    overflow: "hidden",
+    backgroundColor: "#1c2731",
+    borderRadius: 10,
+    marginBottom: 5,
+  },
+  imageBox: {
+    marginLeft: "auto",
+    marginRight: "auto",
+    height: 150,
+    width: 150,
   },
 });
 
